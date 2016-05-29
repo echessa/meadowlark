4f577baed97806416119c83c6aedbb03c45e6e6f
Joyce Echessa <jokhessa@yahoo.com>
2016-05-29 10:25:23 +0300

    Refactor code

--- meadowlark.js
+++ meadowlark.js
@@ -1,10 +1,4 @@
-var fortunes = [
-"Conquer your fears or they will conquer you.",
-"Rivers need springs.",
-"Do not fear what you don't know.",
-"You will have a pleasant surprise.",
-"Whenever possible, keep it simple.",
-];
+var fortune = require('./lib/fortune.js');
 
 var express = require('express');
 
@@ -25,8 +19,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/about', function(req, res) {
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    res.render('about', { fortune: fortune.getFortune() });
 });
 
 // 404 catch-all handler (middleware)
