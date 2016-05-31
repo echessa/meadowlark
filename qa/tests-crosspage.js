ab10e6627398bf004e0e751903d2f9f01e40a234
Joyce Echessa <jokhessa@yahoo.com>
2016-05-31 10:02:13 +0300

    Add unit test file

--- qa/tests-crosspage.js
+++ qa/tests-crosspage.js
@@ -11,27 +11,30 @@ suite('Cross-Page Tests', function() {
     test('requesting a group rate quote from the hood river tour page' +
         ' should populate the referrer field', function(done) {
         var referrer = 'http://localhost:3000/tours/hood-river';
+        this.timeout(15000);
         browser.visit(referrer, function() {
             browser.clickLink('.requestGroupRate', function() {
                 assert(browser.field('referrer').value === referrer);
                 done();
-            })
+            });
         });
     });
 
     test('requesting a group rate quote from the oregon coast tour page' +
         ' should populate the referrer field', function(done) {
         var referrer = 'http://localhost:3000/tours/oregon-coast';
+        this.timeout(15000);
         browser.visit(referrer, function() {
             browser.clickLink('.requestGroupRate', function() {
                 assert(browser.field('referrer').value === referrer);
                 done();
-            })
+            });
         });
     });
 
     test('visiting the "request group rate" page directly should result' +
         ' in an empty referrer field', function(done) {
+        this.timeout(15000);
         browser.visit('http://localhost:3000/tours/request-group-rate', function() {
             assert(browser.field('referrer').value === '');
             done();
